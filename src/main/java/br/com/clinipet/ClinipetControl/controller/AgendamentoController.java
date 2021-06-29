fdc836d53b40b357ba65a9161dd14962efa49391
gsantosdev <santosgustavo431@gmail.com>
2021-06-29 17:41:57 -0300

    Agendamento Estrutura

---
+++ src/main/java/br/com/clinipet/ClinipetControl/controller/AgendamentoController.java
@@ -0,0 +1,39 @@
+package br.com.clinipet.ClinipetControl.controller;
+
+
+import br.com.clinipet.ClinipetControl.model.entity.Agendamento;
+import br.com.clinipet.ClinipetControl.service.AgendamentoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/animais")
+@RequiredArgsConstructor
+public class AgendamentoController {
+
+    private final AgendamentoService agendamentoService;
+
+
+    @PostMapping
+    public ResponseEntity marcar(@RequestBody Agendamento agendamento) {
+        return null;
+    }
+
+    @PutMapping
+    public ResponseEntity remarcar(@RequestBody Agendamento agendamento) {
+        return null;
+    }
+
+    @DeleteMapping
+    public ResponseEntity desmarcar(@RequestBody Agendamento agendamento) {
+        return null;
+    }
+
+
+}
