bbf6d4a5cf1c252cea119a17d74bde226aebe37b
gsantosdev <santosgustavo431@gmail.com>
2021-07-06 22:03:01 -0300

    Alterando consulta de nome

--- src/main/java/br/com/clinipet/ClinipetControl/model/repository/AnimalRepository.java
+++ src/main/java/br/com/clinipet/ClinipetControl/model/repository/AnimalRepository.java
@@ -2,6 +2,8 @@ package br.com.clinipet.ClinipetControl.model.repository;
 
 import br.com.clinipet.ClinipetControl.model.entity.Animal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,7 +12,8 @@ import java.util.Optional;
 @Repository
 public interface AnimalRepository extends JpaRepository<Animal, Long> {
 
-    Optional<List<Animal>> findByNome(String nome);
+    @Query(value = "SELECT * FROM Animal WHERE nome LIKE :nome", nativeQuery = true)
+    Optional<List<Animal>> findByNome(@Param("nome") String nome);
 
 
 }
